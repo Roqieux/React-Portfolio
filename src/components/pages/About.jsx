5793c5dbc9eb4261430ef8cd1746c6cf24983d2e
Julien <larroquejl@gmail.com>
2023-10-24 16:04:13 -0400

    added about.jsx

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,16 @@
+// import stlyes from "../../styles/About.module.css";
+export default function About() {
+    return (
+        <div className="">
+            <h1>About Page</h1>
+            <div className="d-flex mx-3">
+
+                <img src="/RoqieuxStack.png" className={stlyes.img}></img>
+
+                <p className="m-3 fs-5">
+                    After spending 9 years working in Process analytics and improvement with Amazon.com, Julien is currently enrolled in an intensive online full stack web development course. When he's not behind the keys working to improve his development skills, he can be found walking his dog, working on his car, or playing games.
+                </p>
+            </div>
+        </div>
+    );
+}

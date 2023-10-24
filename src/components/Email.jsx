07f0ee0ef52d5157c21a79c32733e48e8e995c84
Julien <larroquejl@gmail.com>
2023-10-24 16:06:27 -0400

    added contact and email form

---
+++ src/components/Email.jsx
@@ -0,0 +1,46 @@
+import {useState} from 'react';
+export default function Form(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [help, setHelp] = useState('')
+
+    const validate = (e) => {
+      e.preventDefault();
+      if (!name || !email || !message) {
+       setHelp( "All fields are required.")
+      } else if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+        setHelp("Invalid email.")
+      } else {
+        location.reload()
+      }
+    };
+
+    const warning = (e) => {
+      e.stopPropagation();
+      if (!e.target.value) {
+        setHelp(`${e.target.id} is required. `)
+      } else if (
+        e.target.id === "Email" &&
+        !e.target.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+      ) {
+        setHelp("Invalid email.")
+      }
+    };
+
+    return (
+        <form className='p-5 col-6'style={{border: "2px solid gray"}}>
+            <label className="form-label" for="name">Name:</label>
+            <input className="form-control" id="Name" type="text" value={name} onChange={e => setName(e.target.value)} onBlur={warning} />
+            <div id="nameHelp" class="form-text"> </div>
+            <label className="form-label" for="email">Email: </label>
+            <input className="form-control" id="Email" type="email"value={email} onChange={e => setEmail(e.target.value)} onBlur={warning}/>
+            <div id="emailHelp" class="form-text"> </div>
+            <label className="form-label" for="message">Message:</label>
+            <textarea className="form-control"  id="Message" value={message} onChange={e => setMessage(e.target.value)} onBlur={warning}/>
+            <div id="help" class="form-text"> {help} </div>
+            
+            <button type='submit' className="btn btn-primary mt-3" onClick={validate}>Send</button>
+        </form>
+    )
+}

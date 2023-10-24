98f829fd658b87ae8852e4c084d117c0dce8e00e
Julien <larroquejl@gmail.com>
2023-10-24 16:05:26 -0400

    added main.jsx routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+import App from './App';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+import Projects from './components/pages/Project';
+import Resume from './components/pages/Resume';
+
+
+// Accessible routes definitions
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Projects />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
